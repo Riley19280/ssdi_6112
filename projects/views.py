403ce0fbef25629effcf139fdde09f9dd7a108b3
Riley Schoppa <rlieyschoppa@yahoo.com>
2020-02-10 16:20:15 -0500

    added view rendering of database content

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from projects.models import Movie
+from django.template import loader
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the projects index for ssdi_6112.")
+
+
+def movies(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies,
+    }
+    return render(request, 'projects/movies.html', context)
