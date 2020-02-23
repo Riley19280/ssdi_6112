b1e6bdd1591a4294e128bf48141d7519e77b2fd7
Riley Schoppa <rlieyschoppa@yahoo.com>
2020-02-22 19:14:41 -0500

    added test file

--- tests/TestHelloWorld.py
+++ tests/TestHelloWorld.py
@@ -1,4 +1,11 @@
 import unittest
+from unittest import mock
+
+
+class TestObject:
+
+    def getHello(self):
+        return 'Hello'
 
 
 class TestHelloWorld(unittest.TestCase):
@@ -6,5 +13,12 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertTrue(1 == 1, "Hello world is working.")
 
+    def test_hello_world_mock(self):
+        with mock.patch.object(TestObject, 'getHello', return_value='mocked'):
+            t = TestObject()
+            self.assertEqual(t.getHello(), 'mocked')
+
+
 if __name__ == '__main__':
     unittest.main()
+
