f1b3d2332db370d6b1414991a01d4f7e062baf23
Riley Schoppa <rlieyschoppa@yahoo.com>
2020-03-28 11:55:24 -0400

    Fix serving of static files and css

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,24 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from . import views
+from django.contrib.staticfiles.views import serve as serve_static
+
+
+
+def _static_butler(request, path, **kwargs):
+    """
+    Serve static files using the django static files configuration
+    WITHOUT collectstatic. This is slower, but very useful for API
+    only servers where the static files are really just for /admin
+
+    Passing insecure=True allows serve_static to process, and ignores
+    the DEBUG=False setting
+    """
+    return serve_static(request, path, insecure=True, **kwargs)
+
 
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
-    path('instructions', views.instructions, name='instructions')
+    path('instructions', views.instructions, name='instructions'),
+    re_path(r'static/(.+)', _static_butler)
 ]
+
